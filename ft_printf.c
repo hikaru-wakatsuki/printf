24a0a81c6da0580589f8c857c45d82971cda0024
Hikaru Wakatsuki <hwakatsu@c5r1s4.42tokyo.jp>
2025-10-24 20:46:18 +0900

    waka

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,51 @@
 /*   By: hwakatsu <hwakatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 19:21:03 by hwakatsu          #+#    #+#             */
-/*   Updated: 2025/10/24 19:43:01 by hwakatsu         ###   ########.fr       */
+/*   Updated: 2025/10/24 20:45:44 by hwakatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdarg.h>
+
+// int	count_n_and_one(const char *format, const char n)
+// {
+// 	int	i;
+
+// 	i = 0;
+// 	while (*format)
+// 	{
+// 		if (*format == n)
+// 		{
+// 			i++;
+// 			format++;
+// 		}
+// 		format++;
+// 	}
+// 	return (i);
+// }
+
+bool	is_valid_flag(const char flag)
+{
+
+}
+
 int	ft_printf(const char *format, ...)
 {
-	
+	char	flag;
+	va_list		ap;
+
+	va_start(ap, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			flag = *(++format);
+			if (is_valid_flag(flag));
+			va_arg(ap, int);
+		}
+		++format;
+
+	}
+	va_end(ap);
+	return (1);
 }
